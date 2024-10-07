7b136bd761841ef7d4c5e718d133704dfa7f483b
ANGGA NIRWANA <88030040+angganrw@users.noreply.github.com>
2024-10-08 01:55:10 +0700

    Update tailwind.config.js

--- EXERCISE-MODUL-02/INTRO-TO-REACT-PORTFOLIO/tailwind.config.js
+++ EXERCISE-MODUL-02/INTRO-TO-REACT-PORTFOLIO/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {},
+       colors: {
+        "extra-color": "#ffdd79", // Custom color
+      },
     },
   },
   plugins: [],
